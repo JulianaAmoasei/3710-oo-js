433c1c3780239a686ebea44d6d8b1fec9e598744
JulianaAmoasei <late.parrot@gmail.com>
2024-05-17 18:14:34 -0300

    video: estrutura da classe

---
+++ User.js
@@ -0,0 +1,19 @@
+class User {
+  constructor(nome, email, nascimento, role, ativo = true) {
+    this.nome = nome;
+    this.email = email;
+    this.nascimento = nascimento;
+    this.role = role || "estudante";
+    this.ativo = ativo;
+  }
+
+  exibirInfos() {
+    return `${this.nome}, ${this.role}`;
+  }
+}
+
+const newUser = new User("Juliana", "j@j.com", "2024-01-01");
+console.log(newUser);
+console.log(newUser.exibirInfos());
+
+console.log(User.prototype.isPrototypeOf(newUser))
